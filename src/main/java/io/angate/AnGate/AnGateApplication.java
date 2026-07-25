070f80d32e6efeb935e825357dcb3fdfba2505d2
arajitdev <arajitdebnathofficial@gmail.com>
2026-07-25 11:32:52 +0530

    first commit

---
+++ src/main/java/io/angate/AnGate/AnGateApplication.java
@@ -0,0 +1,13 @@
+package io.angate.AnGate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnGateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/angate/AnGate/AnGateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.angate.AnGate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
